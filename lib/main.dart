9df843276ddd14febf446db8e8c145779a879fed
Administrator <shikubro90@gmail.com>
2023-06-20 05:06:04 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(appBar: AppBar(
+        title: const Text("ToDo App"),
+      ),
+      body: const SafeArea(
+        child: Center(child: Text("Welcome to ToDo App"))
+        )
+      ,)
+    );
+  }
+}
